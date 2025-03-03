45aab5891b9041bd9be5691626e685d06f1eb716
Ratish Jha <56468086+ratish11@users.noreply.github.com>
2025-03-03 11:05:39 -0500

    Update run_script.sh

--- run_script.sh
+++ run_script.sh
@@ -36,5 +36,5 @@ docker run -d -v chroma-data:/data -e ALLOW_RESET=true -p 8000:8000 --name chrom
 docker run -d -p 8080:8080 --network rapid-demo-network --name rapid-demo ratish11/rapid-demo:1.0.3
 docker run -d -p 3000:3000 --network rapid-demo-network --name rapid-demo-ui ratish11/rapid-demo-ui:1.0.2
 echo "Installations complete, loading model in memory"
-curl -X POST -H "Content-Type: application/json" -d "{'model': ${model_name}, 'prompt': 'What is the capital of France?', 'keep_alive': 15}" http://localhost:11434/api/generate > /dev/null
+curl -X POST -H "Content-Type: application/json" -d "{\"model\": \"${model_name}\", \"prompt\": \"What is the capital of France?\", \"keep_alive\": 15}" http://localhost:11434/api/generate > /dev/null
 
